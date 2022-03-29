46046ce6dcc7cc989c0fc5e2b9f76f23d35a7d5d
zwsimone <zwsimone@gmail.com>
2022-03-29 10:55:12 +0200

    Deploy only to database

--- src/deploy.ts
+++ src/deploy.ts
@@ -173,7 +173,7 @@ export async function deployRealtimeDatabaseRules(
   projectId: string
 ) {
   const deploymentText = await execWithCredentials(
-    ["deploy"],
+    ["deploy", "--only database"],
     projectId,
     gacFilename
   );
